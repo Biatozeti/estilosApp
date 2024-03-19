2cc9e37a8f26fcedacbbf303aaf30ed9b413d8d4
Biatozeti <anabeatriztozetidasilv@gmail.com>
2024-03-19 15:55:06 -0300

    atuaatua

---
+++ App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { FlatList, ScrollView } from 'react-native';
+import ScrollViewExample from './src/ScrollViewExample';
+import FlatListExample from './src/FlatListExample';
+
+function App(): React.JSX.Element {
+  return (
+    <FlatListExample />
+
+  );
+ 
+  }
+export default App;
